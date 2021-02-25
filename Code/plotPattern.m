ac47d5731453e29ac8adbc5acfc2d74049dec49a
Connor <connor.healy@utah.edu>
2021-02-25 11:42:47 -0700

    Moved FixOrientation subfunction to its own file

--- Code/plotPattern.m
+++ Code/plotPattern.m
@@ -82,16 +82,5 @@ set(plotAxes,'FontSize',FontSize)
 
 end
 
-function [pts] = fixOrientation(pts)
-[nrows,ncols] = size(pts);
 
-if ncols~=2 && nrows~=2
-    error('pts must be a 2-column matrix of point coordinates')
-end
-
-if ncols>nrows && nrows==2
-    pts = pts';
-end
-
-end
 
